a51feaf114590a4b347c06b799c5576464fb2419
bjy2000 <52456181+bjy2000@users.noreply.github.com>
2019-07-04 22:09:21 +0800

    Add files via upload

--- sketch_jul04a.ino
+++ sketch_jul04a.ino
@@ -1,92 +1,53 @@
 void setup()
 {
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(3,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
   pinMode(2,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
   Serial.begin(9600);
 }
-int income=0;
+int kb = 0;
 void loop()
 {
-  if(Serial.available()>0)
+  if(Serial.available() > 0)
+  {
+    kb = Serial.read();
+    kb = kb - '0';
+    if(kb&0x1)
     {
-      income=Serial.read();
-        switch(income)
-        {
-          case 'f':
-              forward();
-              break;
-            case 'b':
-              backward();
-              break;
-            case 'l':
-              left();
-                led1();
-              break;
-            case 'r':
-              right();
-                led2();
-              break;
-            case 's':
-              stop();
-              break;
-            default:
-              break;
-        }
+      digitalWrite(7,HIGH);
     }
-}
-
-void forward()
-{
-  digitalWrite(5,HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
-}
-
-void backward()
-{
-  digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(10,HIGH);
-  digitalWrite(9,LOW);
-}
-
-void left()
-{
-  digitalWrite(5,HIGH);
-  digitalWrite(6,LOW);
-  digitalWrite(10,LOW);
-  digitalWrite(9,HIGH);
-}
-
-void right()
-{
-  digitalWrite(6,HIGH);
-  digitalWrite(5,LOW);
-  digitalWrite(9,HIGH);
-  digitalWrite(10,LOW);
-}
-
-void stop()
-{
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(9,LOW);
-  digitalWrite(10,LOW);
-}
-void led1()
-{
-  digitalWrite(3,HIGH);
-  digitalWrite(2,LOW);
-} 
-void led2()
-{
-  digitalWrite(12,HIGH);
-  digitalWrite(13,LOW);
-} 
+    else
+    { 
+      digitalWrite(7,LOW);
+    }  
+    if((kb>>1)%0x1)
+    {
+      digitalWrite(2,HIGH);
+    }
+    else
+    {
+      digitalWrite(2,LOW);
+    }
+    if((kb>>2)%0x1)
+    {
+      digitalWrite(3,HIGH);
+    }
+    else
+    {
+      digitalWrite(3,LOW);
+    }
+    if((kb>>3)&0x1)
+    {
+      digitalWrite(4,HIGH);
+    }
+    else
+    {
+      digitalWrite(4,LOW);
+    }
+    delay(150);
+    digitalWrite(6,HIGH);
+    delay(150);
+  }
+}  
